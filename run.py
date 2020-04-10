cf77414fb0dedce547204d469916ef1ee53b8474
Jovan <jovanshernandez@gmail.com>
2020-04-10 14:07:08 +0000

    first commit

---
+++ run.py
@@ -0,0 +1,12 @@
+# run.py
+
+import os
+
+from app import create_app
+
+#config_name = os.getenv('FLASK_CONFIG')
+config_name = 'development'
+app = create_app(config_name)
+
+if __name__ == '__main__':    
+    app.run(debug=True,host='0.0.0.0',port='8080')
