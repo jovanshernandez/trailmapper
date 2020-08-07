515143796056f56ab6c3cd9557688c502623c165
Ubuntu <ubuntu@ip-172-31-50-133.ec2.internal>
2020-08-07 16:15:06 +0000

    updated requirements file and secured db connection

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:trailmapperpw123@localhost/dreamteam_db'
+SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:trailmapperpw123@localhost/trailmapper_db'
 
 #mysql_newpwd
